9e9e7444c76e4343ba7db45fa3083f6bc27a6415
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-15 15:10:06 -0600

    Clase DbContext, Cadena de conexión e Inyección de dependencias.

--- Seguridad/Program.cs
+++ Seguridad/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Seguridad.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("CONN")));
 
 var app = builder.Build();
 
