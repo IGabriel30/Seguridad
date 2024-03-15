fabbf061c1a1182195c14262b26dac9d375e8f8b
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-15 14:56:23 -0600

    Entidad Usuario

---
+++ Seguridad/Models/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Seguridad.Models
+{
+    public class Usuario
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdUsuario { get; set; }
+
+        public string Nombre { get; set;}
+
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public byte Status { get; set; }
+        
+        public string ConfirmPassword { get; set; }
+    }
+}
