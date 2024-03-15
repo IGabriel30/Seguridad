cdd8133eed03387da162db05de82c8c2ffb6abb2
Lorena Lisseth Vasquez Godoy <2431186@clases.edu.sv>
2024-03-15 15:40:30 -0600

    Uso de anotaciones y migración.

--- Seguridad/Models/Usuario.cs
+++ Seguridad/Models/Usuario.cs
@@ -9,15 +9,25 @@ namespace Seguridad.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdUsuario { get; set; }
 
+        [Required(ErrorMessage ="El nombre de usuario es requerido")]
         public string Nombre { get; set;}
 
+        [Required(ErrorMessage = "El apellido de usuario es requerido")]
         public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "El email de usuario es requerido")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "La contraseña es requerido")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "El estatus es requerido")]
         public byte Status { get; set; }
-        
+
+        [NotMapped]//esta prpiedad no sera mapeada en la base de datos.
+        [Compare("Password", ErrorMessage ="La contraseña no coincide")]
         public string ConfirmPassword { get; set; }
     }
 }
