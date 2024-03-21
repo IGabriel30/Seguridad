49bead01d851dd81c75692440ef125fd8ad34950
Ingrid Jacqueline Gabriel Zepeda <ingridgabriel30@hotmail.com>
2024-03-21 14:26:33 -0600

    Controlador de Reportes.

---
+++ Seguridad/Controllers/ReportesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Seguridad.Controllers
+{
+    [Authorize(Roles ="Administrador")]
+    public class ReportesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
