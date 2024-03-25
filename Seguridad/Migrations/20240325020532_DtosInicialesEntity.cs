05592a333f7438c9ff6024cf09861c831736d708
Ingrid Jacqueline Gabriel Zepeda <ingridgabriel30@hotmail.com>
2024-03-24 20:10:47 -0600

    Implementacion de datos iniciales con Entity(usuario root).

---
+++ Seguridad/Migrations/20240325020532_DtosInicialesEntity.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Seguridad.Migrations
+{
+    public partial class DtosInicialesEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Usuarios",
+                columns: new[] { "IdUsuario", "Apellido", "Email", "Nombre", "Password", "Rol", "Status" },
+                values: new object[] { 1, "admin", "root@gmail.com", "root", "0192023a7bbd73250516f069df18b500", "Administrador", (byte)1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Usuarios",
+                keyColumn: "IdUsuario",
+                keyValue: 1);
+        }
+    }
+}
--- Seguridad/Models/ApplicationDbContext.cs
+++ Seguridad/Models/ApplicationDbContext.cs
@@ -7,5 +7,14 @@ namespace Seguridad.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario { Email = "root@gmail.com", IdUsuario = 1, Nombre = "root", Apellido = "admin", Status = 1, Rol = "Administrador", Password = "0192023a7bbd73250516f069df18b500" }
+                );
+        }
     }
+
+
 }
